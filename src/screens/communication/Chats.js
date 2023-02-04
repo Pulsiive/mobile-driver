11a4228d2c6586bd8d7b3fdf8fa72a4259833d93
Ilias Lamrani <ilias.lamrani@epitech.eu>
2023-02-04 16:43:19 +0900

    feat: add start new discussion button in message interface

--- src/screens/communication/Chats.js
+++ src/screens/communication/Chats.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { FlatList, TextInput } from 'react-native-gesture-handler';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text, Modal, Image } from 'react-native';
 import { AppStyles } from '../../AppStyles';
 
 import Chat from './Chat';
@@ -8,25 +9,41 @@ import api from '../../db/Api';
 
 function Chats() {
   const [chats, setChats] = useState([]);
+  const [contacts, setContacts] = useState([]);
   const [errorMessage, setErrorMessage] = useState();
+  const [newChatModalIsOpen, setNewChatModalIsOpen] = useState(false);
+  const navigation = useNavigation();
 
-  useEffect(() => console.log(chats), [chats]);
+  const profilePicture =
+    'https://vignette.wikia.nocookie.net/krypton-series/images/c/c3/Character-avatar-lyta-zod.png/revision/latest?cb=20180323124150';
 
   useEffect(() => {
-    console.log('getting last messages');
+    api.send('GET', '/api/v1/profile/contacts').then((data) => {
+      setContacts(data.data);
+    });
+  }, []);
+
+  useEffect(() => {
     api
       .send('GET', '/api/v1/profile/messages/last-by-user')
       .then((data) =>
         setChats(
           data.data.map((message) => {
-            message.user.profilePictureUri =
-              'https://vignette.wikia.nocookie.net/krypton-series/images/c/c3/Character-avatar-lyta-zod.png/revision/latest?cb=20180323124150';
+            message.user.profilePictureUri = profilePicture;
             return message;
           })
         )
       )
       .catch((e) => setErrorMessage(e));
   }, []);
+
+  const onContactPress = (contact) => {
+    navigation.navigate('Message', {
+      imageUri: profilePicture,
+      name: `${contact.firstName} ${contact.lastName}`,
+      receiverId: contact.id
+    });
+  };
 
   return (
     <View style={styles.container}>
@@ -45,6 +62,38 @@ function Chats() {
           showsVerticalScrollIndicator={false}
         />
       )}
+      <TouchableOpacity style={styles.roundButton1} onPress={() => setNewChatModalIsOpen(true)}>
+        <Text style={{ color: 'white', fontSize: 30 }}>+</Text>
+      </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        visible={newChatModalIsOpen}
+        onRequestClose={() => setNewChatModalIsOpen(false)}
+        transparent
+      >
+        <View style={styles.modal}>
+          <FlatList
+            data={contacts}
+            keyExtractor={(item) => item.user.id}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  marginBottom: 20
+                }}
+                onPress={() => onContactPress(item.user)}
+              >
+                <Image source={{ uri: profilePicture }} style={{ height: 50, width: 50 }}></Image>
+                <Text
+                  style={{ fontWeight: 'bold', fontSize: 18 }}
+                >{`${item.user.firstName} ${item.user.lastName}`}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -67,6 +116,32 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingRight: 20,
     color: AppStyles.color.text
+  },
+  roundButton1: {
+    width: 70,
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    borderRadius: 100,
+    backgroundColor: AppStyles.color.main
+  },
+  modal: {
+    backgroundColor: AppStyles.color.white,
+    padding: 10,
+    height: '50%',
+    width: '80%',
+    borderRadius: 10,
+    marginTop: '40%',
+    marginLeft: '12%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
   }
 });
 
