dc18ea26213a9961486fa0862e35df36b105bc83
Ilias Lamrani <ilias.lamrani@epitech.eu>
2023-11-01 21:18:55 +0100

    feat: add notification handling

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigator from './src/navigation/AppNavigation';
 import { ThemeProvider } from './src/AppStyles';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 function App() {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert(remoteMessage.notification.title, remoteMessage.notification.body); //TODO: show a flash message when a new reservation was received
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <ThemeProvider>
       <AppNavigator />
